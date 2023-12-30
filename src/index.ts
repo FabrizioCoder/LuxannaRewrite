2b080a6bb2844c2bdff68192478c09e077936f63
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2023-12-30 12:06:28 -0400

    yes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-// import { main } from './app/client';
-// main();
-import 'dotenv/config';
-import { writeFile } from 'fs/promises';
-import { join } from 'path';
-import { SummonersManager } from './app/managers/summonersManager';
-import { makeMatchHistory } from './utils/images/history';
+import { main } from './app/client';
+main();
+// import 'dotenv/config';
+// import { writeFile } from 'fs/promises';
+// import { join } from 'path';
+// import { SummonersManager } from './app/managers/summonersManager';
+// import { makeMatchHistory } from './utils/images/history';
 
-async function draw() {
-    const summoner = (await SummonersManager.getInstance().getSummoner(
-        'lan:FreeAoi:0001'
-    ))!
-    const matchesId = await (await summoner.getMatches()).getMatchesHistory() ?? []
-    const matches = await Promise.all(matchesId.map(x => summoner.match?.getMatchById(x)))
+// async function draw() {
+//     const summoner = (await SummonersManager.getInstance().getSummoner(
+//         'lan:FreeAoi:0001'
+//     ))!
+//     const matchesId = await (await summoner.getMatches()).getMatchesHistory() ?? []
+//     const matches = await Promise.all(matchesId.map(x => summoner.match?.getMatchById(x)))
 
-    console.log(matches)
+//     console.log(matches)
 
-    await writeFile(
-        //@ts-expect-error
-        join(process.cwd(), 'res.png'), await makeMatchHistory(matches.slice(0, 5), summoner)
-    )
-}
+//     await writeFile(
+//         //@ts-expect-error
+//         join(process.cwd(), 'res.png'), await makeMatchHistory(matches.slice(0, 5), summoner)
+//     )
+// }
 
-draw()
+// draw()
